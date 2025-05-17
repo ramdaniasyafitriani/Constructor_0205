fefbd56f5b257672f3091812cda65a198574dcab
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-05-17 16:18:26 +0700

    membuat destructor

--- destruktor.cpp
+++ destruktor.cpp
@@ -23,3 +23,12 @@ angka::angka(int i)
     arr = new int[i];
     isiData();
 }
+
+angka::~angka()
+{ // destructor
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat array sudah dilepaskan " << endl;
+}
+
