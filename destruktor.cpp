b672d753dac680f0a96d7e231b1f57c87ef7b90d
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-05-17 16:24:13 +0700

    memanggil constructor

--- destruktor.cpp
+++ destruktor.cpp
@@ -49,3 +49,12 @@ void angka::isiData()
     }
     cout << endl;
 }
+
+int main()
+{
+    angka belajarcpp(3);                 // constructor dipanggil
+    angka *ptrBelajarcpp = new angka(5); // constructor dipanggil
+    delete ptrBelajarcpp;                // destructor dipanggil
+
+    return 0;
+} // destructor dipanggil
