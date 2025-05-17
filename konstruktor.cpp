a1f9f323b232af890012b6daf0228afb5fd954a7
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-05-17 16:29:50 +0700

    membuat akses modifier public

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -5,3 +5,12 @@ using namespace std;
 class mahasiswa{
 private:
 int nim;
+string nama;
+public:
+mahasiswa();
+mahasiswa(int);
+mahasiswa(string);
+mahasiswa(int iNim, string iNama);
+void cetak();
+};
+
