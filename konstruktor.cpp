3cdd95252e396b13c5c8fbfa81b043f26a4658eb
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-05-17 16:31:49 +0700

    memanggil variabel

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -35,3 +35,17 @@ mahasiswa :: mahasiswa(int iNim, string iNama){
 void mahasiswa::cetak(){
     cout<<endl<<"Nim="<<nim<<endl;
 }
+
+int main(){
+    mahasiswa mhs1;
+    mahasiswa mhs2(20);
+    mahasiswa mhs3("Indra");
+    mahasiswa mhs4(30, "Fauzan");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
+}
