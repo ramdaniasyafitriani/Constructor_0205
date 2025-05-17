c4b95cc4e8f4be635d15338242344088ed6a89b8
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-05-17 16:19:22 +0700

    membuat void angka cetak data

--- destruktor.cpp
+++ destruktor.cpp
@@ -32,3 +32,10 @@ angka::~angka()
     cout << "Alamat array sudah dilepaskan " << endl;
 }
 
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
