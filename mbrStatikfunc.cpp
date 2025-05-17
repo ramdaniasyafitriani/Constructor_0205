465ee0f43cd750eba034cee7e9fcf12502ec66b3
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-05-17 16:38:51 +0700

    membuat definisi function

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -14,3 +14,10 @@ public :
 
     void setID();
     void printAll();
+
+    static void setNim(int pNim){nim = pNim; /*Definisi Function*/}
+    static int getNim(){return nim; /*Definisi Function*/}
+
+    mahasiswa(string pnama):nama(pnama){setID();}
+};
+
